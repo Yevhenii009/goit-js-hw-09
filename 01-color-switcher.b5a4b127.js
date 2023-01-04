94d4f6a4221e4fb13d0d9e982adcb6f4eb5da7de
Yevhenii009 <109582747+Yevhenii009@users.noreply.github.com>
2023-01-04 15:15:49 +0000

    Deploying to gh-pages from @ Yevhenii009/goit-js-hw-09@347f3f051d29e35922164009f1b552613a7ceb03 🚀

---
+++ 01-color-switcher.b5a4b127.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){e.setAttribute("disabled",!0),o.removeAttribute("disabled"),r=setInterval(n,1e3)})),o.addEventListener("click",(function(){e.removeAttribute("disabled"),o.setAttribute("disabled",!0),clearInterval(r)}));let r=null;function n(){const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e}o.setAttribute("disabled",!0);
+//# sourceMappingURL=01-color-switcher.b5a4b127.js.map
