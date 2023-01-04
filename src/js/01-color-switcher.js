347f3f051d29e35922164009f1b552613a7ceb03
Yevhenii009 <stifgun@gmail.com>
2023-01-04 17:15:19 +0200

    дз9-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,88 @@
+const changeBodyColor = document.querySelector('body');
+const startChange = document.querySelector('button[data-start]');
+const stopChange = document.querySelector('button[data-stop]');
 
+startChange.addEventListener('click', getStart);
+stopChange.addEventListener('click', getStop);
+
+let intervalId = null;
+stopChange.setAttribute('disabled', true);
+
+
+function getRandomHexColor() {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    changeBodyColor.style.backgroundColor = randomColor;
+}
+
+function getStart() {
+    startChange.setAttribute('disabled', true);
+    stopChange.removeAttribute('disabled');
+    intervalId = setInterval(getRandomHexColor, 1000);
+}
+
+function getStop() {
+    startChange.removeAttribute('disabled');
+    stopChange.setAttribute('disabled', true);
+    clearInterval(intervalId);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let intervalId = null;
+
+
+// function getRandomHexColor() {
+//     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//     changeBodyColor.style.backgroundColor = randomColor;
+    
+//     intervalId = setInterval(getRandomHexColor, 1000)
+//     startChange.setAttribute('disabled', true);
+    // startChange.setAttribute('disabled', true);
+    
+
+// intervalId = setInterval(getRandomHexColor, 1000)
+
+
+
+
+
+
+
+// function isActive() {
+//     intervalId = setInterval(getRandomHexColor, 1000)
+//     startChange.setAttribute('disabled', true);
+// }
+
+// function notActive() {
+//     clearInterval(intervalId);
+//     stopChange.setAttribute('disabled', true);
+//     startChange.removeAttribute('disabled');
+// }
+
+
+
+
+
+
+
+// function isActive() {
+// if (startChange || stopChange) {
+//         setAttribute('disabled', true);
+// } else {
+//     removeAttribute('disabled');
+//     }
+//
