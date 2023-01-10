83685ce2f2f47c96bb4c672ed6eabd0366c27028
Yevhenii009 <stifgun@gmail.com>
2023-01-10 14:30:25 +0200

    дз9-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+const newForm = document.querySelector('.form');
+newForm.addEventListener('submit', onNewForm);
+
+function onNewForm(elem){
+  elem.preventDefault();
+ 
+  let delay = Number(elem.currentTarget.delay.value);
+  let step = Number(elem.currentTarget.step.value);
+  let amount = Number(elem.currentTarget.amount.value);
+
+for (let position = 1; position <= amount; position += 1) {
+  createPromise(position, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+delay += step;
+}
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  let promiseValue = {position, delay};
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(promiseValue);
+    }
+      reject(promiseValue);
+  })
 }
